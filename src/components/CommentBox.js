2fb719533910ea2ae00a1005bd9b5d46da568e82
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-23 17:33:43 -0300

    feat(comment box): add form, textarea, button

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const CommentBox = () => {
-    return <div>Comment Box</div>
+export default class CommentBox extends Component {
+  state = { comment: "" };
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ comment: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </h4>
+      </form>
+    );
+  }
 }
-
-export default CommentBox
