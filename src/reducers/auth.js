2f040b3b77776db443f37c37a785323d915989be
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-09-25 22:10:20 -0300

    add change auth to reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,9 @@
+import { CHANGE_AUTH } from "actions/types";
+
 const authReducer = function (state = false, action) {
   switch (action.type) {
+    case CHANGE_AUTH:
+      return action.payload;
     default:
       return state;
   }
